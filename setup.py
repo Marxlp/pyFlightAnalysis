967911cdcb07f72e8e75f6485eca58ede35191d5
Marxlp <lipi26@foxmail.com>
2018-05-24 22:15:38 +0800

    update software version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup,find_packages
 
 setup(
         name='pyFlightAnalysis',
-        version='1.0.4',
+        version='1.0.4b1',
         description='Flight log Data analysis visualization tool',
         long_description=open('README.rst').read(),
         url='https://github.com/Marxlp/pyFlightAnalysis',
